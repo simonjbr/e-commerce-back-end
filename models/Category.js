417cf419addf4e931e5e35e99215fa78bb825aab
Simon Brown <simonandon@live.com.au>
2024-05-14 12:36:37 +0800

    Formatted indentation to 4 space tab for all js files

--- models/Category.js
+++ models/Category.js
@@ -2,29 +2,29 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
-  {
-    // define columns
-	id: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-		primaryKey: true,
-		autoIncrement: true,
+	{
+		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		category_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
 	},
-	category_name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'category',
+	}
 );
 
 module.exports = Category;
--- models/Tag.js
+++ models/Tag.js
@@ -2,19 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+	{
+		// define columns
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'tag',
+	}
 );
 
 module.exports = Tag;
