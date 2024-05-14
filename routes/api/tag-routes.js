ef0ffbeb0edc967cc99334112b6cbd333b871a5b
Simon Brown <simonandon@live.com.au>
2024-05-14 14:25:17 +0800

    Added route for GET Tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,9 +19,22 @@ router.get('/', async (req, res) => {
 	}
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
 	// find a single tag by its `id`
 	// be sure to include its associated Product data
+	try {
+		const tagId = req.params.id;
+
+		const tagData = await Tag.findByPk(tagId, { include: Product });
+
+		if (tagData) {
+			res.status(200).json(tagData);
+		} else {
+			res.status(404).json({ message: 'There is no tag data' });
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 router.post('/', (req, res) => {
