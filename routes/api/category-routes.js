476d2559a18e4f5c4b85c1ca141dfe1e26769b78
Simon Brown <simonandon@live.com.au>
2024-05-15 09:42:51 +0800

    Added route for update Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -56,8 +56,30 @@ router.post('/', async (req, res) => {
 	}
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 	// update a category by its `id` value
+	try {
+		const categoryId = req.params.id;
+		const categoryName = req.body.category_name;
+
+		const categoryData = await Category.findByPk(categoryId);
+
+		if (categoryData && categoryName) {
+			const updateCategory = await Category.update({
+				category_name: categoryName,
+			},
+			{
+				where: {
+					id: categoryId
+				}
+			});
+			res.status(200).json(updateCategory);
+		} else {
+			res.status(404).json({ message: 'Failed to update category' });
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 router.delete('/:id', (req, res) => {
